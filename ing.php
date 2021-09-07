c7812fb644f12547b680068c43d35d2c53648914
farcj <41663489+farcj@users.noreply.github.com>
2021-09-07 15:10:56 +0800

    Create ing.php

---
+++ ing.php
@@ -0,0 +1,4 @@
+document.write("https://far.520dy.cn/index.php");
+document.write("https://cdn.jsdelivr.net/gh/farcj/farcjv10/v10guanggao.js");
+document.write("https://cdn.jsdelivr.net/gh/farcj/farcjv10/v10data.js");
+document.write("https://cdn.jsdelivr.net/gh/farcj/farcjv10/update.js");

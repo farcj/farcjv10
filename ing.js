010bb44f71ee1669b53a097d433d18902f378ddb
farcj <41663489+farcj@users.noreply.github.com>
2021-09-07 15:16:52 +0800

    Update ing.js

--- ing.js
+++ ing.js
@@ -1,4 +1,4 @@
-document.write("https://far.520dy.cn/index.php");
-document.write("https://cdn.jsdelivr.net/gh/farcj/farcjv10/v10guanggao.js");
-document.write("https://cdn.jsdelivr.net/gh/farcj/farcjv10/v10data.js");
-document.write("https://cdn.jsdelivr.net/gh/farcj/farcjv10/update.js");
+document.write(`<script src="https://far.520dy.cn/index.php" type="text/javascript" charset="utf-8"></script>`);
+document.write(`<script src="https://cdn.jsdelivr.net/gh/farcj/farcjv10/v10guanggao.js" type="text/javascript" charset="utf-8"></script>`);
+document.write(`<script src="https://cdn.jsdelivr.net/gh/farcj/farcjv10/v10data.js" type="text/javascript" charset="utf-8"></script>`);
+document.write(`<script src="https://cdn.jsdelivr.net/gh/farcj/farcjv10/update.js" type="text/javascript" charset="utf-8"></script>`);
